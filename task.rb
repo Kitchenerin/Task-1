7ae42ec718385e5fce0e7451bca9552d098f7a07
Kitchenerin <lenyajoker66@gmail.com>
2019-03-04 14:07:58 +0300

    Task_1

--- task.rb
+++ task.rb
@@ -1,13 +1,13 @@
-f = File.new("mddrive.txt","w")
+f = File.new("mddrive.txt", "w")
 
 File.open("mddrive.xml") do |f1|
-    while readl = f1.gets
-	name = readl.split(' ')
+  while readl = f1.gets
+    name = readl.split(' ')
 
-    	if  name[0] == "<template"
-		line = name[1].split('"')
-		f.puts(line[1]) 
-    	end
+    if name[0] == "<template"
+      line = name[1].split('"')
+      f.puts(line[1])
     end
+  end
 end
 f.close
